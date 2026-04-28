20df09e16a4dea5ae7eaec4e4cdde9c5e4f3a927
DevAssaf <daviassafmp1@gmail.com>
2026-04-28 11:55:21 -0300

    Delete server directory

--- server/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-// Se for uma requisição OPTIONS (preflight), encerra aqui
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit;
-}
